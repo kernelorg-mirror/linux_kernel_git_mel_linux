f911bdf2bd2c42df3851d85698a8936de1dc9478
Mel Gorman <mgorman@techsingularity.net>
2021-07-23 10:21:03 +0100

    sched: Strengten the skip candidate hint during pick_next_entity
    pick_next_entity will consider the "next buddy" over the highest priority
    task if it's not unfair to do so (as determined by wakeup_preempt_entity).
    However, the way next and last buddies are prioritised completely ignores
    the skip hint.
    
    The potential problem is that an in-kernel user of yield_to() such as
    KVM may explicitly want to yield the current task because it is trying
    to acquire a spinlock from a task that is currently off-cpu but on the
    same runqueue. Ignoring the skip hint potentially means that a KVM thread
    continues to spin uselessly.
    
    This patch modifies the logic such that the skip candidate hint is still
    obeyed if it would not increase fairness. The next buddy will then be
    selected unless its vruntime is too far ahead and yielding to it would
    increase unfairness.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4522,12 +4522,12 @@ pick_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 			se = second;
 	}
 
-	if (cfs_rq->next && wakeup_preempt_entity(cfs_rq->next, left) < 1) {
+	if (cfs_rq->next && wakeup_preempt_entity(cfs_rq->next, se) < 1) {
 		/*
 		 * Someone really wants this to run. If it's not unfair, run it.
 		 */
 		se = cfs_rq->next;
-	} else if (cfs_rq->last && wakeup_preempt_entity(cfs_rq->last, left) < 1) {
+	} else if (cfs_rq->last && wakeup_preempt_entity(cfs_rq->last, se) < 1) {
 		/*
 		 * Prefer last buddy, try to return the CPU to a preempted task.
 		 */
